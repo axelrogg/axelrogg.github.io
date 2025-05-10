2797a18b234adb612d2fead86fd314da6ed3fd44
Axel Rodríguez Chang <axelrogg@icloud.com>
2025-05-10 09:28:42 +0200

    Avoid double language specific subpath in post url

--- src/lib/utils/collections.ts
+++ src/lib/utils/collections.ts
@@ -2,8 +2,9 @@ import type { CollectionEntry } from "astro:content";
 
 export function getBlogCollectionPageParams(page: CollectionEntry<"blog">) {
     const [lang, ...slug] = page.id.split("/");
+    const url = import.meta.env.DEV ? `${lang}/blog/${slug.join("/")}` : `blog/${slug.join("/")}`
     return {
         lang: lang,
-        url: `${lang}/blog/${slug.join("/")}`,
+        url: url,
     };
 }
