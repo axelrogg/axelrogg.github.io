d6244fa8296cd06be734f0551b4ca1bed38bd87c
Axel Rodríguez Chang <axelrogg@icloud.com>
2025-08-22 22:36:42 +0200

    Go back to single language website
    This reverts commit 2797a18b234adb612d2fead86fd314da6ed3fd44.

--- src/lib/utils/collections.ts
+++ src/lib/utils/collections.ts
@@ -2,9 +2,8 @@ import type { CollectionEntry } from "astro:content";
 
 export function getBlogCollectionPageParams(page: CollectionEntry<"blog">) {
     const [lang, ...slug] = page.id.split("/");
-    const url = import.meta.env.DEV ? `${lang}/blog/${slug.join("/")}` : `blog/${slug.join("/")}`
     return {
         lang: lang,
-        url: url,
+        url: `${lang}/blog/${slug.join("/")}`,
     };
 }
